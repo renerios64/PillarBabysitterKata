2404e00489fb26b1375b474c0d0c2ee93b55446f
creign64 <rios.1213@gmail.com>
2017-03-30 18:18:30 -0500

    added start time valid test

--- src/ParsingController.py
+++ src/ParsingController.py
@@ -7,14 +7,15 @@ class ParsingController:
         self.setup_controller()
 
     def setup_controller(self):
-        self.parser.add_argument("start", type=str, help="This is the start time. Using a 24 hr clock.")
-        self.parser.add_argument("end", type=str, help="This is the end time. Using a 24 hr clock.")
+        self.parser.add_argument("start", type=int, help="This is the start time. Using a 24 hr clock.")
+        self.parser.add_argument("end", type=int, help="This is the end time. Using a 24 hr clock.")
 
     def parse(self, command_line_input):
-        self.check_inputs(command_line_input)
+        self._check_inputs(command_line_input)
+
         return self.parser.parse_args(command_line_input)
 
-    def check_inputs(self, command_line_input):
+    def _check_inputs(self, command_line_input):
         if len(command_line_input) > 2:
             exit(100)
         if len(command_line_input) < 2:
--- src/test_ParsingController.py
+++ src/test_ParsingController.py
@@ -12,27 +12,38 @@ class TestParsingController(unittest.TestCase):
         pass
 
     def test_parse_valid_time_in_and_time_out(self):
-        test_input = ['1700', '2300']
+        test_input = [1700, 2300]
         arguments = self.CUT.parse(test_input)
 
-        self.assertEqual(arguments.start, '1700', 'Start time is correct!')
-        self.assertEqual(arguments.end, '2300', 'End time is correct!')
+        self.assertEqual(arguments.start, 1700, 'Start time is correct!')
+        self.assertEqual(arguments.end, 2300, 'End time is correct!')
 
     def test_parse_with_too_many_parameters(self):
-        test_input = ['1700', '2300', 'blah']
+        test_input = [1700, 2300, 'blah']
 
         with self.assertRaises(SystemExit) as cm:
-            arguments = self.CUT.parse(test_input)
+            self.CUT._check_inputs(test_input)
 
         self.assertEqual(cm.exception.code, 100)
 
     def test_parse_with_too_few_paramenters(self):
-        test_input = ['1700']
+        test_input = [1700]
 
         with self.assertRaises(SystemExit) as cm:
-            arguments = self.CUT.parse(test_input)
+            self.CUT._check_inputs(test_input)
 
         self.assertEqual(cm.exception.code, 101)
+
+    def test_invalid_start_time(self):
+        test_input = [1200, 2300]
+
+        with self.assertRaises(SystemExit) as cm:
+            self.CUT._check_inputs(test_input)
+
+        self.assertEqual(cm.exception.code, 102)
+
+
+
 
 
 
