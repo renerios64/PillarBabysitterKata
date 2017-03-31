5ca0617fc537a685578b795850124805769ac046
creign64 <rios.1213@gmail.com>
2017-03-30 23:45:58 -0500

    modified main to execute and deliver the payment amount!

--- src/BabysitterTimeTracker.py
+++ src/BabysitterTimeTracker.py
@@ -11,4 +11,8 @@ if __name__ == "__main__":
     time_tracker()
     parsingController = ParsingController.ParsingController()
     arguments = parsingController.parse(sys.argv[1:])
+    compensation = CompensationCalculator.CompensationCalculator(int(arguments.start), int(arguments.end))
+    compensation.calculate_payment()
 
+    print "Starting at: %s and Ending at: %s" % (arguments.start, arguments.end)
+    print "You earned a total of $%d dollars!" % compensation.payment
