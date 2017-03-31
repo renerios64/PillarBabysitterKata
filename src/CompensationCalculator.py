368a5ebcd096ee230cca64bed27427056da3a2e4
creign64 <rios.1213@gmail.com>
2017-03-30 21:03:19 -0500

    added tests for find hrs after midnight tests pass for find hrs between bedtime and midnight

--- src/CompensationCalculator.py
+++ src/CompensationCalculator.py
@@ -2,14 +2,23 @@ class CompensationCalculator:
     def __init__(self, start_time, end_time):
         self.start_time = start_time
         self.end_time = end_time
+
         self._earliest_time = 1700
         self._latest_time = 500
         self._bedtime = 2000
-        self._midnight = 2400
+        self._midnight24 = 2400
+        self._midnight0 = 0
+
         self._before_bedtime_rate = 12
         self._between_bedtime_and_midnight_rate = 8
         self._after_midnight_rate = 16
+
+        self.before_bedtime_hrs = 0
+        self.between_bedtime_and_midnight_hrs = 0
+        self.after_midnight_hrs = 0
+
         self.before_bedtime_amount = 0
+        self.between_bedtime_and_midnight_amount = 0
         self.after_midnight_amount = 0
         self.payment = 0
 
@@ -22,7 +31,24 @@ class CompensationCalculator:
         else:
             hrs = 0
 
-        self.before_bedtime_amount = hrs
+        self.before_bedtime_hrs = hrs
+
+    def find_hrs_between_bedtime_and_midnight(self):
+        if self._bedtime <= self.start_time <= self._midnight24:
+            if self._bedtime <= self.end_time <= self._midnight24:
+                hrs = (self.end_time - self.start_time) / 100
+            elif self.end_time > self._midnight0:
+                hrs = (self._midnight24 - self.start_time) / 100
+        elif self.start_time < self._bedtime:
+            if self._bedtime <= self.end_time <= self._midnight24:
+                hrs = (self.end_time - self._bedtime) / 100
+            elif self.end_time > self._midnight0:
+                hrs = (self._midnight24 - self._bedtime) / 100
+
+        self.between_bedtime_and_midnight_hrs = hrs
+
+
+
 
 
 
--- src/test_CompensationCalculator.py
+++ src/test_CompensationCalculator.py
@@ -13,41 +13,52 @@ class TestCompensationCalculator(unittest.TestCase):
     def test_find_hrs_before_bedtime(self):
         self.CUT.find_hrs_before_bedtime()
 
-        self.assertEqual(self.CUT.before_bedtime_amount, 3, "3 hours before bedtime")
+        self.assertEqual(self.CUT.before_bedtime_hrs, 3, "3 hours before bedtime")
 
     def test_find_hrs_before_bedtime_when_end_time_is_before_bedtime(self):
         self.CUT = CompensationCalculator.CompensationCalculator(1700, 1900)
 
         self.CUT.find_hrs_before_bedtime()
 
-        self.assertEqual(self.CUT.before_bedtime_amount, 2, "3 hours before bedtime")
+        self.assertEqual(self.CUT.before_bedtime_hrs, 2, "3 hours before bedtime")
 
     def test_find_hrs_before_bedtime_when_end_time_is_at_bedtime(self):
         self.CUT = CompensationCalculator.CompensationCalculator(1700, 2000)
 
         self.CUT.find_hrs_before_bedtime()
 
-        self.assertEqual(self.CUT.before_bedtime_amount, 3, "3 hours before bedtime")
+        self.assertEqual(self.CUT.before_bedtime_hrs, 3, "3 hours before bedtime")
 
-    def test_find_hrs_between_bedtime_and_midnight_when_end_time_is_passed_midnight(self):
+    def test_find_hrs_between_bedtime_and_midnight_when_end_time_is_after_midnight(self):
         self.CUT.find_hrs_between_bedtime_and_midnight()
 
-        self.assertEqual(self.CUT.between_bedtime_and_midnight_amount, 4, "4 hours between bedtime and midnight")
+        self.assertEqual(self.CUT.between_bedtime_and_midnight_hrs, 4, "4 hours between bedtime and midnight")
 
     def test_find_hrs_between_bedtime_and_midnight_when_end_time_is_before_midnight(self):
         self.CUT = CompensationCalculator.CompensationCalculator(1700, 2300)
         self.CUT.find_hrs_between_bedtime_and_midnight()
 
-        self.assertEqual(self.CUT.between_bedtime_and_midnight_amount, 3, "3 hours between bedtime and midnight")
+        self.assertEqual(self.CUT.between_bedtime_and_midnight_hrs, 3, "3 hours between bedtime and midnight")
 
     def test_find_hrs_between_bedtime_and_midnight_when_start_time_is_after_bedtime_and_end_time_is_after_midnight(self):
         self.CUT = CompensationCalculator.CompensationCalculator(2100, 500)
         self.CUT.find_hrs_between_bedtime_and_midnight()
 
-        self.assertEqual(self.CUT.between_bedtime_and_midnight_amount, 3, "3 hours between bedtime and midnight")
+        self.assertEqual(self.CUT.between_bedtime_and_midnight_hrs, 3, "3 hours between bedtime and midnight")
 
     def test_find_hrs_between_bedtime_and_midnight_when_start_time_is_after_bedtime_and_end_time_is_before_midnight(self):
         self.CUT = CompensationCalculator.CompensationCalculator(2100, 2300)
         self.CUT.find_hrs_between_bedtime_and_midnight()
 
-        self.assertEqual(self.CUT.between_bedtime_and_midnight_amount, 2, "4 hours between bedtime and midnight")
+        self.assertEqual(self.CUT.between_bedtime_and_midnight_hrs, 2, "4 hours between bedtime and midnight")
+
+    def test_find_hrs_after_midnight_when_start_time_is_before_midnight(self):
+        self.CUT.find_hrs_after_midnight()
+
+        self.assertEqual(self.CUT.after_midnight_hrs, 2, "2 hours after midnight")
+
+    def test_find_hrs_after_midnight_when_start_time_is_after_midnight(self):
+        self.CUT = CompensationCalculator.CompensationCalculator(100, 500)
+        self.CUT.find_hrs_between_bedtime_and_midnight()
+
+        self.assertEqual(self.CUT.between_bedtime_and_midnight_hrs, 4, "4 hours after midnight")
