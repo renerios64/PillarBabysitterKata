01f510417cd8df36937b402747ca10e262da69e2
creign64 <rios.1213@gmail.com>
2017-03-30 19:00:03 -0500

    adding before bedtime calculation test

--- src/CompensationCalculator.py
+++ src/CompensationCalculator.py
@@ -1,3 +1,14 @@
 class CompensationCalculator:
-    def __init__(self):
-        pass
+    def __init__(self, start_time, end_time):
+        self.start_time = start_time
+        self.end_time = end_time
+        self.bedtime = 2000
+        self.before_bedtime_rate = 12
+        self.between_bedtime_and_midnight_rate = 8
+        self.after_midnight_rate = 16
+        self._before_bedtime_amount = 0
+
+
+
+
+
--- src/test_CompensationCalculator.py
+++ src/test_CompensationCalculator.py
@@ -5,7 +5,15 @@ import CompensationCalculator
 
 class TestCompensationCalculator(unittest.TestCase):
     def setUp(self):
-        self.CUT = CompensationCalculator.CompensationCalculator()
+        self.CUT = CompensationCalculator.CompensationCalculator(1700, 200)
 
     def tearDown(self):
         pass
+
+    def test_cost_before_bedtime(self):
+        self.CUT._before_bedtime_calculation()
+
+        self.assertEqual(self.CUT.get_before_bedtime_amount(), 36, "3 hours from start to bedtime = 36 dollars")
+
+
+
