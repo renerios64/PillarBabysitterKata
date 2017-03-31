b617e2356a84695ac0160603ed0e7762a89f8c40
creign64 <rios.1213@gmail.com>
2017-03-30 20:14:17 -0500

    testing and passing hrs_before_bedtime

--- src/CompensationCalculator.py
+++ src/CompensationCalculator.py
@@ -2,6 +2,8 @@ class CompensationCalculator:
     def __init__(self, start_time, end_time):
         self.start_time = start_time
         self.end_time = end_time
+        self._earliest_time = 1700
+        self._latest_time = 500
         self._bedtime = 2000
         self._midnight = 2400
         self._before_bedtime_rate = 12
@@ -10,6 +12,23 @@ class CompensationCalculator:
         self.before_bedtime_amount = 0
         self.after_midnight_amount = 0
         self.payment = 0
+
+    def find_hrs_before_bedtime(self):
+        if self._earliest_time <= self.start_time <= self._bedtime:
+            if self._earliest_time <= self.end_time <= self._bedtime:
+                hrs = (self.end_time - self.start_time) / 100
+            else:
+                hrs = (self._bedtime - self.start_time) / 100
+        else:
+            hrs = 0
+
+        self.before_bedtime_amount = hrs
+
+
+
+
+
+
 
 
 
--- src/test_CompensationCalculator.py
+++ src/test_CompensationCalculator.py
@@ -11,6 +11,20 @@ class TestCompensationCalculator(unittest.TestCase):
         pass
 
     def test_find_hrs_before_bedtime(self):
-        self.CUT.find_hrs_brefore_bedtime()
+        self.CUT.find_hrs_before_bedtime()
 
-        self.assertEqual(self.CUT.hrs_before_bedtime, 3, "3 hours before bedtime")
+        self.assertEqual(self.CUT.before_bedtime_amount, 3, "3 hours before bedtime")
+
+    def test_find_hrs_before_bedtime_when_end_time_is_before_bedtime(self):
+        self.CUT = CompensationCalculator.CompensationCalculator(1700, 1900)
+
+        self.CUT.find_hrs_before_bedtime()
+
+        self.assertEqual(self.CUT.before_bedtime_amount, 2, "3 hours before bedtime")
+
+    def test_find_hrs_before_bedtime_when_end_time_is_at_bedtime(self):
+        self.CUT = CompensationCalculator.CompensationCalculator(1700, 2000)
+
+        self.CUT.find_hrs_before_bedtime()
+
+        self.assertEqual(self.CUT.before_bedtime_amount, 3, "3 hours before bedtime")
