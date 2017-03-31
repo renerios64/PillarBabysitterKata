ec1954c3eed07701e1f97359de7a207fab0d5771
creign64 <rios.1213@gmail.com>
2017-03-30 19:59:15 -0500

    restarting! wasn't going down the right path.

--- src/CompensationCalculator.py
+++ src/CompensationCalculator.py
@@ -9,22 +9,12 @@ class CompensationCalculator:
         self._after_midnight_rate = 16
         self.before_bedtime_amount = 0
         self.after_midnight_amount = 0
+        self.payment = 0
 
-    def _before_bedtime_calculation(self):
-        elapse_time = (self.bedtime - self.start_time) / 100
-        self.before_bedtime_amount = elapse_time * self._before_bedtime_rate
 
-    def _after_midnight_calculation(self):
-        elapse_time = self.end_time / 100
-        self.after_midnight_amount = elapse_time * self._after_midnight_rate
 
-    def _between_bedtime_and_midnight_calculation(self):
-        if 0 < self.end_time < 500:
-            elapse_time = (self._midnight - self._bedtime) / 100
-            self.between_bedtime_and_midnight_amount = elapse_time * self._between_bedtime_and_midnight_rate
-        else:
-            elapse_time = (self.end_time - self._bedtime) / 100
-            self.between_bedtime_and_midnight_amount = elapse_time * self._between_bedtime_and_midnight_rate
+
+
 
 
 
--- src/test_CompensationCalculator.py
+++ src/test_CompensationCalculator.py
@@ -10,30 +10,7 @@ class TestCompensationCalculator(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_cost_before_bedtime(self):
-        self.CUT._before_bedtime_calculation()
+    def test_find_hrs_before_bedtime(self):
+        self.CUT.find_hrs_brefore_bedtime()
 
-        self.assertEqual(self.CUT.before_bedtime_amount, 36, "3 hours from start to bedtime = 36 dollars")
-
-    def test_cost_after_midnight(self):
-        self.CUT._after_midnight_calculation()
-
-        self.assertEqual(self.CUT.after_midnight_amount, 32, "2 hours after midnight = 32 dollars")
-
-    def test_cost_between_bedtime_and_midnight(self):
-        self.CUT._between_bedtime_and_midnight_calculation()
-
-        self.assertEqual(self.CUT.between_bedtime_and_midnight_amount, 32,
-                         "4 hours between bedtime and midnight = 32 dollars")
-
-    def test_cost_between_bedtime_and_midnight_when_end_time_is_before_midnight(self):
-        self.CUT = CompensationCalculator.CompensationCalculator(1700, 2300)
-        self.CUT._between_bedtime_and_midnight_calculation()
-
-        self.assertEqual(self.CUT.between_bedtime_and_midnight_amount, 24,
-                         "3 hours between bedtime and midnight = 24 dollars")
-
-    def test_total_amount_to_collect(self):
-        self.CUT.payment_calculator()
-
-        self.assertEqual(self.CUT.payment, 100, "From 1700 until 500 the cost = 100")
+        self.assertEqual(self.CUT.hrs_before_bedtime, 3, "3 hours before bedtime")
